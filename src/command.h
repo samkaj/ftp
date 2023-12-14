68688564c1f9f6ef8f7586ffe3f8574475675577
samkaj <kajavasamuel@gmail.com>
2023-12-14 22:26:35 +0100

    create enum for commands and add helpers

---
+++ src/command.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <string>
+#include <ostream>
+
+enum class Command;
+
+Command command_from_string(std::string const& command);
+std::string command_to_string(Command command);
+std::ostream& operator<<(std::ostream& os, const Command& command);
