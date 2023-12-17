96f49a60d6179347853465b19e7301205920f689
samkaj <kajavasamuel@gmail.com>
2023-12-17 13:15:02 +0100

    don't parse blank lines as commands

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,10 @@ void read_commands()
 {
     std::string line{};
     while (std::getline(std::cin, line)) {
+        if (line.empty()) {
+            std::cout << "> ";
+            continue;
+        }
         Request request(line);
         std::cout << command_to_string(request.command) << std::endl;
         std::cout << "> ";
