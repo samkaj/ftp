6508647320b2f00d2714dd0e6e4b4996cd2003ab
samkaj <kajavasamuel@gmail.com>
2023-12-17 15:39:55 +0100

    bloat gone

--- src/command.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <string>
-#include <ostream>
-
-enum class Command;
-
-Command command_from_string(std::string const& command);
-std::string command_to_string(Command command);
